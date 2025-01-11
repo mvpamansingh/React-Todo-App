8c028af04ac6518f939eea939fef98ca2ccb5c1d
mvpamansingh <amancodes5@gmail.com>
2025-01-12 00:46:46 +0530

    Some more code - of validation in updateTodo and createTodo API

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 app.use(cors());
@@ -12,11 +13,36 @@ app.get("/getAllTodos", (req,res)=>{
 
 app.post("/updateTodo", (req,res)=>{
 
+    const recevedID= req.body;
+    const parsedId= updateTodo.safeParse(recevedID);
+
+    if(!parsedId.success)
+    {
+        res.status(411).json({
+            msg:"wrong input"
+        })
+        return;
+    }
+
+    
+
 })
 
 
 app.post("/createTodo", (req,res)=>{
+    const recevedPayload = req.body;
 
+    const parsedPayload = createTodo.safeParse(recevedPayload);
+
+    if(!parsedPayload.success)
+    {
+        res.status(411).json({
+            msg:"Wrong data send"
+        })
+        return;
+    }
+
+    // insert document in mongodb
 })
 
 app.listen(8000,(err)=>{
