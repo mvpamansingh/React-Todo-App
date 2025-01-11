aaa5a2fde13974a76c53bc9a5beffec246bfacbb
mvpamansingh <amancodes5@gmail.com>
2025-01-12 01:46:04 +0530

    Completed the backend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 
 const app = express();
 app.use(cors());
 
 
-app.get("/getAllTodos", (req,res)=>{
+app.get("/getAllTodos", async (req,res)=>{
 
+    try{
+
+        const todoList = await todo.find({});
+
+        res.json({
+            todolist:todoList
+        })
+        
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
 
 })
 
-app.post("/updateTodo", (req,res)=>{
+app.post("/updateTodo", async(req,res)=>{
 
-    const recevedID= req.body;
+    const recevedID= req.body.id; 
     const parsedId= updateTodo.safeParse(recevedID);
 
     if(!parsedId.success)
@@ -24,12 +38,24 @@ app.post("/updateTodo", (req,res)=>{
         return;
     }
 
-    
+    try{
+        const getTodo = await todo.find({_id :parsedId})
+
+        const status = getTodo.completed
+        getTodo.completed = !status;
+        await getTodo.save();
+
+        res.json(getTodo);
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
 
 })
 
 
-app.post("/createTodo", (req,res)=>{
+app.post("/createTodo", async (req,res)=>{
     const recevedPayload = req.body;
 
     const parsedPayload = createTodo.safeParse(recevedPayload);
@@ -43,6 +69,15 @@ app.post("/createTodo", (req,res)=>{
     }
 
     // insert document in mongodb
+
+    await todo.create({
+        title: parsedPayload.data.title,
+        description: parsedPayload.data.description,
+        completed: false
+    })
+    res.status(500).json({
+        msg:"Todo Created Successfully "
+    })
 })
 
 app.listen(8000,(err)=>{
