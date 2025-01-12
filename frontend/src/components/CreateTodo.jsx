2a35b7d45413d62a0a320266419e6cee1081a890
mvpamansingh <amancodes5@gmail.com>
2025-01-12 13:06:10 +0530

    Designed skeleton for displaying todo list, state - future update

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,19 +1,36 @@
 
- function CreateTodo()
+ export function CreateTodo()
 {
     return <div>
 
-        <input type = "text" placeholder="Title of your Todo" ></input>
+        <input style= {{
+            padding:10,
+            margin:10
+        }} type = "text" placeholder="Title of your Todo" ></input>
         <br/>
         <br/>
-        <input type ="text" placeholder="description"></input>
+        <input style ={{
+            padding:10,
+            margin:10
+        }} type ="text" placeholder="description"></input>
         <br/>
         <br/>
 
-        <button onClick={""}> Add Todo</button>
+        <button style ={{
+            padding:10,
+            margin:10
+        }}   onClick={""}> Add Todo</button>
     </div>
 }
 
-module.exports = {
-    CreateTodo
-}
+// this syntax is wrong as react follows ES6 
+/*
+In your code, you were using CommonJS style (module.exports) but React uses ES6 modules. Here's why this matters:
+Vite (your build tool) uses ES modules by default
+React components are typically written using ES6 module syntax
+Modern browsers support ES modules natively
+
+*/
+// module.exports = {
+//     CreateTodo
+// }
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,10 @@
+
+
+export function Todos(){
+    return <div>
+        
+        <h1>Go to Gym</h1>
+        <h2>You need to </h2>
+        <button>mark as completed</button>
+    </div>
+}
