ddc3372884f7fd66b11b947cf9f9d7c0d183f36b
mvpamansingh <amancodes5@gmail.com>
2025-01-12 12:25:06 +0530

    Frontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,19 @@
+
+ function CreateTodo()
+{
+    return <div>
+
+        <input type = "text" placeholder="Title of your Todo" ></input>
+        <br/>
+        <br/>
+        <input type ="text" placeholder="description"></input>
+        <br/>
+        <br/>
+
+        <button onClick={""}> Add Todo</button>
+    </div>
+}
+
+module.exports = {
+    CreateTodo
+}
